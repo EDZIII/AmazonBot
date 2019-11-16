853575fe896a0799bd718e820e0af735d7bc6929
EDZIII <edo.cre@live.it>
2019-11-16 21:52:58 +0100

    boh

---
+++ AmaBot/Classes/Laptop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmaBot
+{
+	class Laptop
+	{
+		public string Date { get; set; }
+		public string Title { get; set; }
+		public string Price { get; set; }
+		public string Href { get; set; }
+	}
+}
